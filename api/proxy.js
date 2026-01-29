fd37ce1cb3205c22960219da48f4a236f61e9264
Clyde56 <74910202+Clyde56@users.noreply.github.com>
2026-01-30 01:18:39 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://cn.pornhub.com/view_video.php?viewkey=66457c90ce7ff";//your website url
+  let target = "https://chatgpt.com/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
@@ -15,9 +15,9 @@ module.exports = (req, res) => {
     target,
     changeOrigin: true,
     pathRewrite: {
-      // rewrite request path `/backend`
-      //  /backend/user/login => http://google.com/user/login
-      //   "^/backend/": "/",
+       rewrite request path `/backend`
+        /backend/user/login => https://chatgpt.com/user/login
+        "^/backend/": "/",
     },
   })(req, res);
 };
